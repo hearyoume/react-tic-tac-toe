be1dac23ef34ed86e072fe28a292bd1dbf5126a0
Amanda Ellis <amanda.ellis002@student.swic.edu>
2025-10-26 14:33:40 -0500

    Update calculateWinner to return winner and winning indices

--- src/lib.js
+++ src/lib.js
@@ -1,8 +1,12 @@
 import { WINNING_INDICES } from "./constants.js";
 
-export const calculateWinner = (board) =>
-  ["X", "O"].find((letter) =>
-    WINNING_INDICES.some((winningIndices) =>
-      winningIndices.every((winningIndex) => board[winningIndex] === letter)
-    )
-  ) || null;
+export const calculateWinner = (board) => {
+  for (const letter of ["X", "O"]) {
+    for (const winningIndices of WINNING_INDICES) {
+      if (winningIndices.every((index) => board[index] === letter)) {
+        return { winner: letter, line: winningIndices };
+      }
+    }
+  }
+  return { winner: null, line: [] };
+};
